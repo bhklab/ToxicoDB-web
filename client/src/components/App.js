2d2801b9bd5f7306ad1cffc7e3ed34c74d2eb529
chuntul <chantal.ho98@hotmail.com>
2019-11-01 17:26:00 -0400

    it breaks but gotta commit anyway

---
+++ client/src/components/App.js
@@ -0,0 +1,46 @@
+import React, {Fragment} from 'react';
+import {Route, Switch } from 'react-router-dom'
+import styled from 'styled-components';
+import Landing from './Landing';
+import Genes from './Genes';
+import Datasets from './Datasets';
+import Drugs from './Drugs';
+import HeaderNav from './HeaderNav';
+import FooterNav from './FooterNav';
+import Documentation from './Documentation';
+
+const StyledApp = styled.div`
+  margin: 0 auto;
+  // padding: 12.5vh 0 25px;
+  min-height:100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  flex-grow: 1;
+  min-width: 300px;
+  max-width: 1200px;
+  overflow-x:hidden;
+  order:2;
+  z-index:1;
+`;
+
+const App = () => {
+  return (
+    <Fragment>
+      <StyledApp className="app">
+        <Switch>
+          {/* <Route exact path="/" component={Landing} />
+          <Route exact path="/genes/" component={Genes} />
+          <Route exact path="/drugs/" component={Drugs} />
+          <Route exact path="/datasets/" component={Datasets} />
+          <Route exact path="/documentation/" component={Documentation} /> */}
+        </Switch>
+      </StyledApp>
+      <HeaderNav/>
+      <FooterNav/>
+    </Fragment>
+  );
+}
+
+export default App;
