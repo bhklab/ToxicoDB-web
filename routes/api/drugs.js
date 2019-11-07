b4efd7c81d920aab2ec63a6db096cc7555973cea
gangeshberiuhn <gangesh.beri@uhnresearch.ca>
2019-11-07 12:34:10 -0500

    getDrugs function to get the list of drugs

--- routes/api/drugs.js
+++ routes/api/drugs.js
@@ -3,9 +3,14 @@ const knex = require('../../db/knex');
 
 
 // get all the data from the drugs table.
-const getdrugs = function (request, response) {
+const getdrugs = (request, response) => {
     knex.select()
         .from('drugs')
+        .leftJoin(
+            'drug_annotations',
+            'drugs.id',
+            'drug_annotations.drug_id',
+        )
         .then((drug) => response.status(200).json({
             status: 'success',
             data: drug,
