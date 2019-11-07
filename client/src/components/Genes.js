8b7e9f61247fa7f6630fdba603c511dd48f5a670
Denis Tkachuk <deventorum@gmail.com>
2019-11-07 14:56:53 -0500

    Added react table and ncbi links

--- client/src/components/Genes.js
+++ client/src/components/Genes.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 // import { Link } from 'react-router-dom';
 // import styled from 'styled-components';
 // import colors from '../styles/colors';
+import ReactTable from 'react-table';
+import 'react-table/react-table.css';
 
 class Genes extends Component {
   constructor() {
@@ -13,12 +15,11 @@ class Genes extends Component {
 
   componentDidMount() {
     fetch('/api/v1/genes')
-      .then((response) => {
-        console.log(response);
-        return response.json();
-      })
+      .then((response) => response.json())
       .then((res) => {
-        console.log(res);
+        const { data } = res;
+        console.log(data);
+        this.setState({ geneData: data });
         //   const drugsData = data.map(item => ({ value: item.idDrug, label: item.name }));
         //   this.setState({ drugsData1: [{ value: 'Any', label: 'Any Compound' }, ...drugsData] });
         //   this.setState({ drugsData2: [{ value: 'Any', label: 'Any Compound' }, ...drugsData] });
@@ -27,8 +28,35 @@ class Genes extends Component {
   }
 
   render() {
+    const { geneData } = this.state;
+    console.log(geneData);
+    const columns = [{
+      Header: 'Name',
+      accessor: 'name',
+      sortable: true,
+    }, {
+      Header: 'Source',
+      accessor: 'entrez_id',
+      sortable: false,
+      Cell: (props) => {
+        console.log(props);
+        return (<a className="hover" target="_blank" rel="noopener noreferrer" href={`https://www.ncbi.nlm.nih.gov/gene/?term=${props.value}`}>NCBI</a>);
+      },
+    }];
+
     return (
-      <div>Gene Table</div>
+      <div>
+        <h1>List of Genes</h1>
+        { geneData ? (
+          <ReactTable
+            data={geneData}
+            columns={columns}
+            className="-highlight"
+            defaultPageSize={7}
+          />
+        ) : null}
+
+      </div>
     );
   }
 }
