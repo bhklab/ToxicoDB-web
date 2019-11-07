b615a0717d0ce99e7cfea977caa7c9c68aee7c29
Denis Tkachuk <deventorum@gmail.com>
2019-11-07 15:55:57 -0500

    Cleaned up dependencies

--- client/src/components/Genes.js
+++ client/src/components/Genes.js
@@ -1,9 +1,34 @@
 import React, { Component } from 'react';
 // import { Link } from 'react-router-dom';
-// import styled from 'styled-components';
+import styled from 'styled-components';
 // import colors from '../styles/colors';
 import ReactTable from 'react-table';
 import 'react-table/react-table.css';
+
+const StyledWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  background:white;
+  padding:0px 30px;
+
+  #dsetMiniPlot {
+    margin-left:40px;
+  }
+
+  #dsetLegend {
+    margin: 20px 0px 0px 40px;
+    
+    &span {
+      line-height:5px;
+      font-size:85%;
+    }
+    
+  }
+ 
+`;
 
 class Genes extends Component {
   constructor() {
@@ -32,16 +57,18 @@ class Genes extends Component {
     console.log(geneData);
     const columns = [{
       Header: 'Name',
+      accessor: 'hgnc_id',
+      sortable: true,
+    }, {
+      Header: 'Ensembl ID',
       accessor: 'name',
       sortable: true,
+      Cell: (props) => <a className="hover" target="_blank" rel="noopener noreferrer" href={`http://useast.ensembl.org/Homo_sapiens/Gene/Summary?g=${props.value}`}>{props.value}</a>,
     }, {
       Header: 'Source',
       accessor: 'entrez_id',
       sortable: false,
-      Cell: (props) => {
-        console.log(props);
-        return (<a className="hover" target="_blank" rel="noopener noreferrer" href={`https://www.ncbi.nlm.nih.gov/gene/?term=${props.value}`}>NCBI</a>);
-      },
+      Cell: (props) => <a className="hover" target="_blank" rel="noopener noreferrer" href={`https://www.ncbi.nlm.nih.gov/gene/?term=${props.value}`}>{props.value}</a>,
     }];
 
     return (
