518e7dea3cad1aed401bdc0204f3e4539ebfef35
gangeshberiuhn <gangesh.beri@uhnresearch.ca>
2019-11-08 14:07:49 -0500

    migration samples and viabilities

---
+++ db/migrations/20191108135507_samples.js
@@ -0,0 +1,27 @@
+/* eslint-disable func-names */
+/* eslint-disable no-unused-vars */
+
+exports.up = function (knex, Promise) {
+    return knex.schema.createTable('samples', (table) => {
+        table.increments('id')
+            .primary();
+        table.integer('drug_id')
+            .notNullable()
+            .unsigned()
+            .references('id')
+            .inTable('drugs')
+            .index();
+        table.integer('cell_id')
+            .notNullable()
+            .unsigned()
+            .references('id')
+            .inTable('cells')
+            .index();
+        table.string('name')
+            .notNullable();
+    });
+};
+
+exports.down = function (knex, Promise) {
+    return knex.schema.dropTable('samples');
+};
---
+++ db/migrations/20191108135517_viabilities.js
@@ -0,0 +1,24 @@
+/* eslint-disable func-names */
+/* eslint-disable no-unused-vars */
+
+exports.up = function (knex, Promise) {
+    return knex.schema.createTable('viabilities', (table) => {
+        table.integer('sample_id')
+            .unsigned()
+            .unique()
+            .notNullable()
+            .references('id')
+            .inTable('samples')
+            .index();
+        table.string('viability')
+            .notNullable();
+        table.string('dose')
+            .notNullable();
+        table.string('time')
+            .notNullable();
+    });
+};
+
+exports.down = function (knex, Promise) {
+    return knex.schema.dropTable('viabilities');
+};
