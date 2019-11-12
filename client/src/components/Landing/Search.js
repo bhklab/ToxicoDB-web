8b88211e3142e8a0ee8360125196c73c07487c13
Denis Tkachuk <deventorum@gmail.com>
2019-11-12 16:49:31 -0500

    fixed dependencies

--- client/src/components/Landing/Search.js
+++ client/src/components/Landing/Search.js
@@ -1,52 +1,52 @@
 import React, { Component, Fragment } from 'react';
 import styled from 'styled-components';
-import colors from '../../styles/colors';
 import Select from 'react-select';
+import colors from '../../styles/colors';
 import MenuList from './MenuList';
 
 const customStyles = {
-    control: provided => ({
-      ...provided,
-      background: 'rgb(0,0,0,0)',
+  control: (provided) => ({
+    ...provided,
+    background: 'rgb(0,0,0,0)',
+    border: `1px solid ${colors.nav_links}`,
+    margin: '5px 0px',
+    '&:hover': {
       border: `1px solid ${colors.nav_links}`,
-      margin: '5px 0px',
-      '&:hover': {
-        border: `1px solid ${colors.nav_links}`,
-        cursor: 'text',
-      },
-    }),
-    placeholder: provided => ({
-      ...provided,
+      cursor: 'text',
+    },
+  }),
+  placeholder: (provided) => ({
+    ...provided,
+    color: `${colors.nav_links}`,
+  }),
+  dropdownIndicator: (provided) => ({
+    ...provided,
+    color: `${colors.nav_links}`,
+    '&:hover': {
       color: `${colors.nav_links}`,
-    }),
-    dropdownIndicator: provided => ({
-      ...provided,
-      color: `${colors.nav_links}`,
-      '&:hover': {
-        color: `${colors.nav_links}`,
-        cursor: 'pointer',
-      },
-    }),
-  
-    indicatorSeparator: provided => ({
-      ...provided,
+      cursor: 'pointer',
+    },
+  }),
+
+  indicatorSeparator: (provided) => ({
+    ...provided,
+    background: `${colors.nav_links}`,
+    '&:hover': {
       background: `${colors.nav_links}`,
-      '&:hover': {
-        background: `${colors.nav_links}`,
-      },
-    }),
-    singleValue: provided => ({
-      ...provided,
-      color: `${colors.nav_links}`,
-    }),
-    option: (provided, state) => ({
-      ...provided,
-      textAlign: state.isDisabled ? 'left' : 'center',
-      fontWeight: state.isDisabled ? '700' : state.isSelected ? '700' : '400',
-      background: state.isDisabled ? colors.summary_bg : 'white',
-      color: state.isSelected ? colors.color_main_2 : colors.nav_links,
-    }),
-  };
+    },
+  }),
+  singleValue: (provided) => ({
+    ...provided,
+    color: `${colors.nav_links}`,
+  }),
+  option: (provided, state) => ({
+    ...provided,
+    textAlign: state.isDisabled ? 'left' : 'center',
+    fontWeight: state.isDisabled ? '700' : state.isSelected ? '700' : '400',
+    background: state.isDisabled ? colors.summary_bg : 'white',
+    color: state.isSelected ? colors.color_main_2 : colors.nav_links,
+  }),
+};
 
 
 const StyledSearch = styled.div`
@@ -76,86 +76,82 @@ const StyledSearch = styled.div`
 `;
 
 class Search extends Component {
-    constructor() {
-        super();
-        this.state = {
-            searchData: [],
-        }
-    }
+  constructor() {
+    super();
+    this.state = {
+      searchData: [],
+    };
+  }
 
-    componentDidMount() {
-        // let searchData = [
-        //     {
-        //         label: "Drugs",
-        //         options: [],
-        //     }, {
-        //         label: "Genes",
-        //         options: [],
-        //     }
-        // ];
-        let searchData = [];
+  componentDidMount() {
+    // let searchData = [
+    //     {
+    //         label: "Drugs",
+    //         options: [],
+    //     }, {
+    //         label: "Genes",
+    //         options: [],
+    //     }
+    // ];
+    let searchData = [];
 
-        fetch('/api/v1/drugs')
-            .then(response => response.json())
-            .then((data) => {
-                let drugData = data.data.map(x => {
-                    return {
-                        label: x.name.charAt(0).toUpperCase() + x.name.slice(1),
-                        value: x.name
-                    }
-                })
-                // adding drug data to searchData drug options
-                // searchData[0].options = drugData;
-                searchData = searchData.concat(drugData)
-                this.setState({ searchData: searchData});
-        });
+    fetch('/api/v1/drugs')
+      .then((response) => response.json())
+      .then((data) => {
+        const drugData = data.data.map((x) => ({
+          label: x.name.charAt(0).toUpperCase() + x.name.slice(1),
+          value: x.name,
+        }));
+        // adding drug data to searchData drug options
+        // searchData[0].options = drugData;
+        searchData = searchData.concat(drugData);
+        this.setState({ searchData });
+      });
 
-        fetch('/api/v1/genes')
-            .then(response => response.json())
-            .then((data) => {
-                let geneData = data.data.map(x => {
-                    return {
-                        label: x.name.charAt(0).toUpperCase() + x.name.slice(1),
-                        value: x.name
-                    }
-                })
-                // adding genes data to searchData drug options
-                // searchData[1].options = geneData;
-                searchData = searchData.concat(geneData)
-                this.setState({ searchData: searchData});
-        });
+    fetch('/api/v1/genes')
+      .then((response) => response.json())
+      .then((data) => {
+        const geneData = data.data.map((x) => ({
+          label: x.name.charAt(0).toUpperCase() + x.name.slice(1),
+          value: x.name,
+        }));
+        // adding genes data to searchData drug options
+        // searchData[1].options = geneData;
+        searchData = searchData.concat(geneData);
+        this.setState({ searchData });
+      });
+  }
 
-    }
-    render() {
-        const {searchData} = this.state;
-        console.log(searchData)
-        const customFilterOption = (option, rawInput) => {
-            const words = rawInput.split(' ');
-                return words.reduce(
-                  (acc, cur) => acc && option.label.toLowerCase().includes(cur.toLowerCase()),
-                  true,
-                );
-        };
-        return (
-            // <StyledSearch>
-            //     <input type="text" className="input" placeholder="Search..." />
-            // </StyledSearch>
-            <Fragment>
-                {searchData.length == 0 ? null : (
-                    <Select
+  render() {
+    const { searchData } = this.state;
+    console.log(searchData);
+    const customFilterOption = (option, rawInput) => {
+      const words = rawInput.split(' ');
+      return words.reduce(
+        (acc, cur) => acc && option.label.toLowerCase().includes(cur.toLowerCase()),
+        true,
+      );
+    };
+    return (
+    // <StyledSearch>
+    //     <input type="text" className="input" placeholder="Search..." />
+    // </StyledSearch>
+      <>
+        {searchData.length == 0 ? null : (
+              <Select
                         // isMulti
                         // filterOption={customFilterOption}
-                        options={searchData}
+                    options={searchData}
                         // components={{ MenuList }}
-                        placeholder="Enter Cell Line or Tissue"
-                        styles={customStyles}
-                    />
-                )}
-            </Fragment>
-            
-            
-        )
-    }
+                    placeholder="Enter Cell Line or Tissue"
+                    styles={customStyles}
+                  />
+            )}
+      </>
+
+
+    );
+  }
 }
 
 export default Search;
