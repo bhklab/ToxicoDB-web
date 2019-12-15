abf6be29440f5ba3b3b112acb3c5dee626c6e1d6
gangeshberiuhn <gangesh.beri@uhnresearch.ca>
2019-12-15 13:35:54 -0500

    Formatting APIs

--- routes/api/drugs.js
+++ routes/api/drugs.js
@@ -4,7 +4,7 @@ const knex = require('../../db/knex');
 
 // get all the data from the drugs table.
 const getDrugs = (request, response) => {
-    knex.select('id', 'name', 'symbol','pubchem', 'chembl', 'drugbank', 'carcinogenicity', 'class_in_vitro', 'class_in_vivo', 'targets', 'class_name', 'smiles', 'inchikey')
+    knex.select('id', 'name', 'symbol', 'pubchem', 'chembl', 'drugbank', 'carcinogenicity', 'class_in_vitro', 'class_in_vivo', 'targets', 'class_name', 'smiles', 'inchikey')
         .from('drugs')
         .leftJoin(
             'drug_annotations',
@@ -22,14 +22,14 @@ const getDrugs = (request, response) => {
 };
 
 const getIndivDrug = (request, response) => {
-    knex.select('id','name', 'symbol','pubchem', 'chembl', 'drugbank', 'carcinogenicity', 'class_in_vitro', 'class_in_vivo', 'targets', 'class_name', 'smiles', 'inchikey')
+    knex.select('id', 'name', 'symbol', 'pubchem', 'chembl', 'drugbank', 'carcinogenicity', 'class_in_vitro', 'class_in_vivo', 'targets', 'class_name', 'smiles', 'inchikey')
         .from('drugs')
         .leftJoin(
             'drug_annotations',
             'drugs.id',
             'drug_annotations.drug_id',
         )
-        .where({ "drugs.id": request.params.id })
+        .where({ 'drugs.id': request.params.id })
         .then((drug) => response.status(200).json({
             status: 'success',
             data: drug,
@@ -38,27 +38,27 @@ const getIndivDrug = (request, response) => {
             status: 'could not find data from drugs table, getDrugs',
             data: error,
         }));
-}
+};
 
 const getDrugAnalysis = (request, response) => {
-    knex.distinct("fdr", "fold_change", "p_value", "genes.id AS gene_id", "genes.name AS gene_name", "datasets.name AS dataset_name")
-        .from("analysis")
-        .innerJoin("drug_gene_response AS dgr", "analysis.id", "dgr.analysis_id")
-        .innerJoin("samples", "samples.id", "dgr.sample_id")
-        .innerJoin("genes", "dgr.gene_id", "genes.id")
-        .innerJoin("datasets_samples AS ds", "samples.id", "ds.sample_id")
-        .innerJoin("datasets", "ds.dataset_id", "datasets.id")
-        .where({"drug_id": request.params.id})
-    .then((analysis) => response.status(200).json({
-        status: 'success',
-        data: analysis,
-    }))
-    .catch((error) => response.status(500).json({
-        status: 'could not find data from analysis table, getDrugAnalysis',
-        data: error,
-    }));
-}
-    
+    knex.distinct('fdr', 'fold_change', 'p_value', 'genes.id AS gene_id', 'genes.name AS gene_name', 'datasets.name AS dataset_name')
+        .from('analysis')
+        .innerJoin('drug_gene_response AS dgr', 'analysis.id', 'dgr.analysis_id')
+        .innerJoin('samples', 'samples.id', 'dgr.sample_id')
+        .innerJoin('genes', 'dgr.gene_id', 'genes.id')
+        .innerJoin('datasets_samples AS ds', 'samples.id', 'ds.sample_id')
+        .innerJoin('datasets', 'ds.dataset_id', 'datasets.id')
+        .where({ drug_id: request.params.id })
+        .then((analysis) => response.status(200).json({
+            status: 'success',
+            data: analysis,
+        }))
+        .catch((error) => response.status(500).json({
+            status: 'could not find data from analysis table, getDrugAnalysis',
+            data: error,
+        }));
+};
+
 module.exports = {
-    getDrugs, getIndivDrug, getDrugAnalysis
+    getDrugs, getIndivDrug, getDrugAnalysis,
 };
