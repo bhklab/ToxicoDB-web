3187895d42203f72b75bcc80e9eeab2f0b0d6922
gangeshberiuhn <gangesh.beri@uhnresearch.ca>
2019-12-15 14:02:14 -0500

    Starting creating an Object for the Dataset Description

---
+++ client/src/components/SummaryPages/DatasetDescription.js
@@ -0,0 +1,9 @@
+export const DatasetDescription = {
+    TGGATES_Human_LDH: {
+        Acronym: 'Toxicogenomics Project-Genomics Assisted Toxicity Evaluation system',
+    },
+};
+
+export default {
+    DatasetDescription,
+};
--- client/src/components/SummaryPages/Datasets.js
+++ client/src/components/SummaryPages/Datasets.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import ReactTable from 'react-table';
 import colors from '../../styles/colors';
