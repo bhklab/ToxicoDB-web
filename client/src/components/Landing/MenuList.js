6f71b0f4f034bae280d725fa74710519ff7c1ac7
Denis Tkachuk <deventorum@gmail.com>
2020-01-14 15:50:24 -0500

    Hooks implementation for menulist

--- client/src/components/Landing/MenuList.js
+++ client/src/components/Landing/MenuList.js
@@ -1,9 +1,6 @@
-import React, { PureComponent } from 'react';
+import React, { useState, useEffect } from 'react';
 import { List } from 'react-virtualized/dist/commonjs/List';
 import styled from 'styled-components';
-
-
-// const height = 50;
 
 const StyledList = styled(List)`
     background: #fff;
@@ -11,69 +8,57 @@ const StyledList = styled(List)`
     overflow: hidden
 `;
 
-export default class MenuList extends PureComponent {
-    constructor(props) {
-        super(props);
-        this.state = {
-            width: 0,
-            height: 0,
-        };
-        this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
-    }
+const getWidth = () => {
+    const { innerWidth: width } = window;
+    return width;
+};
 
-    componentDidMount() {
-        this.updateWindowDimensions();
-        window.addEventListener('resize', this.updateWindowDimensions);
-    }
+const MenuList = (props) => {
+    const [width, setWidth] = useState(getWidth());
 
-    componentWillUnmount() {
-        window.removeEventListener('resize', this.updateWindowDimensions);
-    }
-
-    updateWindowDimensions() {
-        this.setState({ width: window.innerWidth, height: window.innerHeight });
-    }
-
-    render() {
-        const { width, height } = this.state;
-        console.log(width, height);
-        const {
-            options, children, maxHeight, getValue,
-        } = this.props;
-        const [value] = getValue();
-        const initialOffset = options.indexOf(value) * 50;
-
-        const rows = Array.isArray(children) ? children : [];
-
-        function rowRenderer({
-            key, index, style,
-        }) {
-            return (
-                <div
-                    key={key}
-                    style={style}
-                >
-                    {rows[index]}
-                </div>
-            );
+    useEffect(() => {
+        function handleResize() {
+            setWidth(getWidth());
         }
 
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    const {
+        options, children, maxHeight, getValue,
+    } = props;
+    const [value] = getValue();
+    const initialOffset = options.indexOf(value) * 50;
+
+    const rows = Array.isArray(children) ? children : [];
+
+    function rowRenderer({
+        key, index, style,
+    }) {
         return (
-            <StyledList
-                ref={(el) => {
-                    this.StyledList = el;
-                }}
-                height={maxHeight}
-                width={width * 0.8}
-                rowCount={rows.length}
-                rowHeight={50}
-                rowRenderer={rowRenderer}
-                itemCount={children.length}
-                initialScrollOffset={initialOffset}
-                // {...props}
+            <div
+                key={key}
+                style={style}
             >
-                {({ index, style }) => <div style={style}>{children[index]}</div>}
-            </StyledList>
+                {rows[index]}
+            </div>
         );
     }
-}
+
+    return (
+        <StyledList
+            height={maxHeight}
+            width={width * 0.8}
+            rowCount={rows.length}
+            rowHeight={50}
+            rowRenderer={rowRenderer}
+            itemCount={children.length}
+            initialScrollOffset={initialOffset}
+        >
+            {({ index, style }) => <div style={style}>{children[index]}</div>}
+        </StyledList>
+    );
+};
+
+export default MenuList;
