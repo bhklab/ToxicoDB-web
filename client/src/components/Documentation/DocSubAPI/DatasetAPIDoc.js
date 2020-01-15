db6238e6ca50839e849f2ebd99845f8a82fbaf95
Denis Tkachuk <deventorum@gmail.com>
2020-01-15 12:15:37 -0500

    Dataset API component

--- client/src/components/Documentation/DocSubAPI/DatasetAPIDoc.js
+++ client/src/components/Documentation/DocSubAPI/DatasetAPIDoc.js
@@ -1,38 +1,20 @@
 /* eslint-disable max-len */
 import React from 'react';
 
-const allDrugData = {
+const allDatasetData = {
     status: 'success',
     data: [
         {
             id: 1,
-            name: '1-Naphthyl isothiocyanate',
-            symbol: 'ANIT',
-            pubchem: 11080,
-            chembl: '',
-            drugbank: '',
-            carcinogenicity: '',
-            class_in_vitro: 'NGTX',
-            class_in_vivo: '',
-            targets: '',
-            class_name: '',
-            smiles: 'C1=CC=C2C(=C1)C=CC=C2N=C=S',
-            inchikey: 'JBDOSUUXMYMWQH-UHFFFAOYSA-N',
+            name: 'TGGATES Human LDH',
         },
         {
             id: 2,
-            name: 'Phenylbutazone',
-            symbol: 'PhB',
-            pubchem: 4781,
-            chembl: '',
-            drugbank: '',
-            carcinogenicity: 'C',
-            class_in_vitro: 'GTX',
-            class_in_vivo: 'NGTX',
-            targets: '',
-            class_name: '',
-            smiles: 'CCCCC1C(=O)N(N(C1=O)C2=CC=CC=C2)C3=CC=CC=C3',
-            inchikey: 'VYMDGNCVAMGZFE-UHFFFAOYSA-N',
+            name: 'TGGATES Rat LDH',
+        },
+        {
+            id: 3,
+            name: 'drugMatrix',
         },
     ],
 };
@@ -58,38 +40,8 @@ const drugData = {
     ],
 };
 
-const drugAnalysis = {
-    status: 'success',
-    data: [
-        {
-            fdr: '0.0140394900967384',
-            fold_change: -0.26868251,
-            p_value: '0.00275455691716522',
-            gene_id: 3935,
-            gene_name: 'MSH3',
-            dataset_name: 'TGGATES Human LDH',
-        },
-        {
-            fdr: '0.0140665335551909',
-            fold_change: -0.39425924,
-            p_value: '0.00276156045048297',
-            gene_id: 3937,
-            gene_name: 'GABRG2',
-            dataset_name: 'TGGATES Human LDH',
-        },
-        {
-            fdr: '0.0140665335551909',
-            fold_change: -0.35523966,
-            p_value: '0.00276196694955832',
-            gene_id: 3938,
-            gene_name: 'CCNG1',
-            dataset_name: 'TGGATES Human LDH',
-        },
-    ],
-};
 
-
-const DrugApiDoc = () => (
+const DatasetApiDoc = () => (
     <div className="doc">
         <h1>API Documentation</h1>
         <p>
@@ -97,51 +49,16 @@ const DrugApiDoc = () => (
             {' '}
              supports RESTful API which allows users to directly query the database without having to use web app interface
         </p>
-        <h2>Drug API</h2>
+        <h2>Dataset API</h2>
         <div className="api-section">
-            <h3>Get list of drugs</h3>
-            <p>Retrieves data for all drugs that have been used in the analysis</p>
+            <p>Retrieves data about all available datasets</p>
             <p className="code">
-                <span>curl http://toxicodb.ca/api/v1/drugs</span>
+                <span>curl http://toxicodb.ca/api/v1/datasets</span>
             </p>
             <p>Output: </p>
-            <div><pre className="output">{JSON.stringify(allDrugData, null, 2)}</pre></div>
-        </div>
-        <div className="api-section">
-            <h3>Get Specific Drug</h3>
-            <p>
-                Retrieves information for a given drug. The API request requires
-                {' '}
-                <em>drug id</em>
-                {' '}
-                (integer)
-                {' '}
-                request paramater.
-            </p>
-            <p className="code">
-                <span>curl http://toxicodb.ca/api/v1/drugs/72</span>
-            </p>
-            <p>Output: </p>
-            <div><pre className="output">{JSON.stringify(drugData, null, 2)}</pre></div>
-        </div>
-        <div className="api-section">
-            <h3>Get Drug Analysis Data</h3>
-            <p>
-                Retrieves analysis data for a given drug across all genes. The API requires
-                {' '}
-                <em>drug id</em>
-                {' '}
-                (integer)
-                {' '}
-                to process the request.
-            </p>
-            <p className="code">
-                <span>curl http://toxicodb.ca/api/v1/drugs/1/analysis</span>
-            </p>
-            <p>Output: </p>
-            <div><pre className="output">{JSON.stringify(drugAnalysis, null, 2)}</pre></div>
+            <div><pre className="output">{JSON.stringify(allDatasetData, null, 2)}</pre></div>
         </div>
     </div>
 );
 
-export default DrugApiDoc;
+export default DatasetApiDoc;
