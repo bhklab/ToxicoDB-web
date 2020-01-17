db8f167959214a960ef38129bbe723bf35f9f757
Denis Tkachuk <deventorum@gmail.com>
2020-01-17 17:05:06 -0500

    CSV download now supports filtering + fixed problem when filtering didn't work properly for numeric values

--- client/src/components/IndivPages/Hooks/useFetchAnalysisData.js
+++ client/src/components/IndivPages/Hooks/useFetchAnalysisData.js
@@ -2,21 +2,39 @@ import { useState, useEffect } from 'react';
 
 // this function is called whenever url changed
 const useFetchAnalysisData = (url) => {
-    const [{ analysisData, loading }, setAnalysisData] = useState({
-        analysisData: [], loading: true,
+    const [{
+        analysisData,
+        loading,
+        //   filteredData
+    }, setAnalysisData] = useState({
+        analysisData: [],
+        loading: true,
+        //   filteredData: [],
     });
     useEffect(() => {
-        setAnalysisData({ analysisData: [], loading: true });
+        setAnalysisData({
+            analysisData: [],
+            loading: true,
+            //  filteredData: []
+        });
         fetch(url)
             .then((response) => response.json())
             .then((res) => {
                 const { data } = res;
-                const filteredData = data.filter((item) => parseFloat(item.p_value) !== 0 && item.dataset_name !== 'drugMatrix');
-                setAnalysisData({ analysisData: filteredData, loading: false });
+                const updatedData = data.filter((item) => parseFloat(item.p_value) !== 0 && item.dataset_name !== 'drugMatrix');
+                setAnalysisData({
+                    analysisData: updatedData,
+                    loading: false,
+                    // filteredData: updatedData,
+                });
             });
     }, [url]);
 
-    return { analysisData, loading };
+    return [{
+        analysisData,
+        loading,
+        //   filteredData
+    }, setAnalysisData];
 };
 
 export default useFetchAnalysisData;
