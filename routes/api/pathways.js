d978806f5fbdce7bd399e14deac9c0b86049ba0b
gangeshberiuhn <gangesh.beri@uhnresearch.ca>
2020-03-05 14:38:14 -0500

    pathways api new function.

--- routes/api/pathways.js
+++ routes/api/pathways.js
@@ -16,7 +16,30 @@ const getPathways = function (request, response) {
         }));
 };
 
+// get the pathways based on dataset and drug.
+const getPathwaysPerDatasetDrug = function (request, response) {
+    const { drugName } = request.body;
+    const { datasetName } = request.body;
+
+    knex.select()
+        .from('pathways_drugs')
+        .innerJoin('datasets', 'datasets.id', 'pathways_drugs.dataset_id')
+        .innerJoin('drugs', 'drugs.id', 'pathways_drugs.drug_id')
+        .innerJoin('pathways', 'pathways.id', 'pathways_drugs.pathway_id')
+        .where('datasets.name', datasetName)
+        .andWhere('drugs.name', drugName)
+        .then((pathways) => response.status(200).json({
+            status: 'success',
+            data: pathways,
+        }))
+        .catch((error) => response.status(500).json({
+            status: 'could not find data from pathways table, getPathwaysPerDatasetDrug',
+            data: error,
+        }));
+};
+
 
 module.exports = {
     getPathways,
+    getPathwaysPerDatasetDrug,
 };
