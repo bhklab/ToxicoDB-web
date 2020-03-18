9dbcdb1feb79038b5952e2ddb998602e27004359
Chantal Ho <chantal.ho98@hotmail.com>
2020-03-18 11:53:55 -0400

    bigger font sizes

--- client/src/components/Plots/Expression.js
+++ client/src/components/Plots/Expression.js
@@ -1,8 +1,8 @@
 import * as d3 from 'd3';
 import React from 'react';
 
-String.prototype.replaceAll = function(search, replacement) {
-    var target = this;
+String.prototype.replaceAll = function (search, replacement) {
+    const target = this;
     return target.replace(new RegExp(search, 'g'), replacement);
 };
 
@@ -13,7 +13,7 @@ class Expression extends React.Component {
 
     componentDidMount() {
         const {
-        data, plotId, xRange, yRange, datasets
+            data, plotId, xRange, yRange, datasets,
         } = this.props;
 
         this.plotExpression(data, plotId, xRange, yRange, datasets);
@@ -21,13 +21,13 @@ class Expression extends React.Component {
 
 
     plotExpression(data, plotId, xRange, yRange, datasets) {
-        console.log(xRange, yRange)
+        console.log(xRange, yRange);
         // positions and dimensions
         const margin = {
-        top: 20,
-        right: 200,
-        bottom: 90,
-        left: 70,
+            top: 20,
+            right: 200,
+            bottom: 90,
+            left: 70,
         };
 
         const width = 1000;
@@ -88,197 +88,184 @@ class Expression extends React.Component {
             .call(xAxis)
             .selectAll('text')
             .attr('fill', 'black')
-            .style('font-size', 14)
+            .style('font-size', 15)
             .attr('stroke', 'none');
 
         // Y axis label
-        svg.append("text")
-            .attr("text-anchor", "middle")
-            .attr("fill","black")
-            .attr("font-family", "Arial")
-            .attr("transform", "translate("+ -40 +","+(height/2)+")rotate(-90)")
-            .text("Expression");
+        svg.append('text')
+            .attr('text-anchor', 'middle')
+            .attr('fill', 'black')
+            .attr('font-family', 'Arial')
+            .attr('transform', `translate(${-40},${height / 2})rotate(-90)`)
+            .text('Expression');
 
         // X axis label
-        svg.append("text")
-            .attr("text-anchor", "middle")
-            .attr("fill","black")
-            .attr("font-family", "Arial")
-            .attr("transform", "translate("+ (width/2) +","+(height+50)+")")
-            .text("Time (hrs)");
-        
+        svg.append('text')
+            .attr('text-anchor', 'middle')
+            .attr('fill', 'black')
+            .attr('font-family', 'Arial')
+            .attr('transform', `translate(${width / 2},${height + 50})`)
+            .text('Time (hrs)');
+
         // line function
         const line = d3.line()
-            .x(d => xrange(d.time))
-            .y(d => yrange(d.exp));
+            .x((d) => xrange(d.time))
+            .y((d) => yrange(d.exp));
 
 
-        let legend = svg.append("g")
-            
+        const legend = svg.append('g');
+
 
         // making the paths
-        data.forEach((t,i) => {
+        data.forEach((t, i) => {
             // make lines
-            svg.append("path")
-                .attr("class", `${t.class}-path`)
-                .attr("d", line(t.points))
-                .attr("fill", "none")
-                .attr("stroke", t.color)
-                .attr("opacity", 1)
-                .attr("stroke-width", 4)
-                .attr("stroke-dasharray", () => {
-                    if (t.mode == "solid") return "";
-                    else return t.mode;
-                });   
-        // make dots
-            let dots = svg.selectAll("dot")
+            svg.append('path')
+                .attr('class', `${t.class}-path`)
+                .attr('d', line(t.points))
+                .attr('fill', 'none')
+                .attr('stroke', t.color)
+                .attr('opacity', 1)
+                .attr('stroke-width', 4)
+                .attr('stroke-dasharray', () => {
+                    if (t.mode == 'solid') return '';
+                    return t.mode;
+                });
+            // make dots
+            const dots = svg.selectAll('dot')
                 .data(t.points)
                 .enter();
 
-                // add dots for those that are below 100 for response
-                dots.append("circle")
-                    .attr("r", 4)
-                    .attr("class", `${t.class}-path`)
-                    .attr("opacity", 1)
-                    .attr("fill", t.color)
-                    .attr("cx", function(d) {return xrange(d.time);})
-                    .attr("cy", function(d) {return yrange(d.exp);})
+            // add dots for those that are below 100 for response
+            dots.append('circle')
+                .attr('r', 4)
+                .attr('class', `${t.class}-path`)
+                .attr('opacity', 1)
+                .attr('fill', t.color)
+                .attr('cx', (d) => xrange(d.time))
+                .attr('cy', (d) => yrange(d.exp));
 
             // to make sure it only puts a legend once
-            if (t.class == datasets[0].replaceAll(' ','')) {
+            if (t.class == datasets[0].replaceAll(' ', '')) {
                 // legend
-                legend.append("line")
-                    .attr("class", `${t.class} legLine`)
-                    .attr("x1", width + 10)
-                    .attr("y1", 50 + (i*21))
-                    .attr("x2", width + 45)
-                    .attr("y2", 50 + (i*21))
-                    .attr("fill", "none")
-                    .attr("stroke", t.color)
-                    .style("opacity", 1)
-                    .attr("stroke-width", 4)
-                    .attr("stroke-dasharray", () => {
-                        if (t.mode == "solid") return "";
-                        else return t.mode;
-                    })
+                legend.append('line')
+                    .attr('class', `${t.class} legLine`)
+                    .attr('x1', width + 10)
+                    .attr('y1', 50 + (i * 21))
+                    .attr('x2', width + 45)
+                    .attr('y2', 50 + (i * 21))
+                    .attr('fill', 'none')
+                    .attr('stroke', t.color)
+                    .style('opacity', 1)
+                    .attr('stroke-width', 4)
+                    .attr('stroke-dasharray', () => {
+                        if (t.mode == 'solid') return '';
+                        return t.mode;
+                    });
 
-                legend.append("text")
-                    .attr("class", `${t.class} legLabel`)
-                    .attr("fill", "black")
-                    .style("font-size", 13)
-                    .attr("font-family", "Arial")
-                    .attr("x", width + 50)
-                    .attr("y", 55 + (i*21))
-                    .text(t.label)
+                legend.append('text')
+                    .attr('class', `${t.class} legLabel`)
+                    .attr('fill', 'black')
+                    .style('font-size', 15)
+                    .attr('font-family', 'Arial')
+                    .attr('x', width + 50)
+                    .attr('y', 55 + (i * 21))
+                    .text(t.label);
             }
-            
-        })
+        });
 
         // making the dataset selectors
-        let nest = d3.nest()
-            .key(function(d) {
-                return d;
-            })
+        const nest = d3.nest()
+            .key((d) => d)
             .entries(datasets);
 
 
-
-        nest.forEach((d,i) => {
+        nest.forEach((d, i) => {
             legend.append('rect')
-                .attr("x", width+10)
-                .attr("y", height - 80 + (i*20))
-                .attr("class", `${datasets[i].replaceAll(' ','')}-legRect`)
-                .attr("width", 13)
-                .attr("height", 13)
-                .attr("fill", () => {
+                .attr('x', width + 10)
+                .attr('y', height - 80 + (i * 20))
+                .attr('class', `${datasets[i].replaceAll(' ', '')}-legRect`)
+                .attr('width', 13)
+                .attr('height', 13)
+                .attr('fill', () => {
                     if (datasets[i] != datasets[0]) {
-                        d3.selectAll(`.${datasets[i].replaceAll(' ','')}-path`).attr("opacity", 0)
-                        d3.selectAll(`.${datasets[i].replaceAll(' ','')}-legRect`).attr("fill", "white")
+                        d3.selectAll(`.${datasets[i].replaceAll(' ', '')}-path`).attr('opacity', 0);
+                        d3.selectAll(`.${datasets[i].replaceAll(' ', '')}-legRect`).attr('fill', 'white');
                         return 'white';
-                    } else {
-                        return 'black';
                     }
+                    return 'black';
                 })
-                .style("stroke", "black")
-                .style("stroke-width", 1)
-                .style("cursor", "pointer")
-                .on("click", () => {
-                    let active   = d.active ? false : true
+                .style('stroke', 'black')
+                .style('stroke-width', 1)
+                .style('cursor', 'pointer')
+                .on('click', () => {
+                    const active = !d.active;
 
                     // only show first dataset first
                     if (datasets[i] != datasets[0]) {
-                        //to show that this dataset has been selected
+                        // to show that this dataset has been selected
                         if (active) {
-                            d3.selectAll(`.${datasets[i].replaceAll(' ','')}-path`).attr("opacity", 1)
-                            d3.selectAll(`.${datasets[i].replaceAll(' ','')}-legRect`).attr("fill", "black")
+                            d3.selectAll(`.${datasets[i].replaceAll(' ', '')}-path`).attr('opacity', 1);
+                            d3.selectAll(`.${datasets[i].replaceAll(' ', '')}-legRect`).attr('fill', 'black');
                         } else {
-                            d3.selectAll(`.${datasets[i].replaceAll(' ','')}-path`).attr("opacity", 0)
-                            d3.selectAll(`.${datasets[i].replaceAll(' ','')}-legRect`).attr("fill", "white")
+                            d3.selectAll(`.${datasets[i].replaceAll(' ', '')}-path`).attr('opacity', 0);
+                            d3.selectAll(`.${datasets[i].replaceAll(' ', '')}-legRect`).attr('fill', 'white');
                         }
-                    
+
                         d.active = active;
                     } else {
-                        //to show that this dataset has been selected
+                        // to show that this dataset has been selected
                         if (active) {
-                            d3.selectAll(`.${datasets[i].replaceAll(' ','')}-path`).attr("opacity", 0)
-                            d3.selectAll(`.${datasets[i].replaceAll(' ','')}-legRect`).attr("fill", "white")
+                            d3.selectAll(`.${datasets[i].replaceAll(' ', '')}-path`).attr('opacity', 0);
+                            d3.selectAll(`.${datasets[i].replaceAll(' ', '')}-legRect`).attr('fill', 'white');
                         } else {
-                            d3.selectAll(`.${datasets[i].replaceAll(' ','')}-path`).attr("opacity", 1)
-                            d3.selectAll(`.${datasets[i].replaceAll(' ','')}-legRect`).attr("fill", "black")
+                            d3.selectAll(`.${datasets[i].replaceAll(' ', '')}-path`).attr('opacity', 1);
+                            d3.selectAll(`.${datasets[i].replaceAll(' ', '')}-legRect`).attr('fill', 'black');
                         }
-                    
+
                         d.active = active;
                     }
-        
-                     
-                })
+                });
 
-            legend.append("text")
-                .attr("class", `${datasets[i]}-legDsetLabel`)
-                .attr("fill", "black")
-                .style("font-size", 13)
-                .attr("font-family", "Arial")
-                .attr("x", width + 30)
-                .attr("y", height - 69 + (i*20))
-                .style("cursor", "pointer")
-                .on("click", () => {
-                    let active   = d.active ? false : true
-        
+            legend.append('text')
+                .attr('class', `${datasets[i]}-legDsetLabel`)
+                .attr('fill', 'black')
+                .style('font-size', 15)
+                .attr('font-family', 'Arial')
+                .attr('x', width + 30)
+                .attr('y', height - 69 + (i * 20))
+                .style('cursor', 'pointer')
+                .on('click', () => {
+                    const active = !d.active;
+
                     // only show TGGATEs human first
                     if (datasets[i] != datasets[0]) {
-                        //to show that this dataset has been selected
+                        // to show that this dataset has been selected
                         if (active) {
-                            d3.selectAll(`.${datasets[i].replaceAll(' ','')}-path`).attr("opacity", 1)
-                            d3.selectAll(`.${datasets[i].replaceAll(' ','')}-legRect`).attr("fill", "black")
+                            d3.selectAll(`.${datasets[i].replaceAll(' ', '')}-path`).attr('opacity', 1);
+                            d3.selectAll(`.${datasets[i].replaceAll(' ', '')}-legRect`).attr('fill', 'black');
                         } else {
-                            d3.selectAll(`.${datasets[i].replaceAll(' ','')}-path`).attr("opacity", 0)
-                            d3.selectAll(`.${datasets[i].replaceAll(' ','')}-legRect`).attr("fill", "white")
+                            d3.selectAll(`.${datasets[i].replaceAll(' ', '')}-path`).attr('opacity', 0);
+                            d3.selectAll(`.${datasets[i].replaceAll(' ', '')}-legRect`).attr('fill', 'white');
                         }
-                    
+
                         d.active = active;
                     } else {
-                        //to show that this dataset has been selected
+                        // to show that this dataset has been selected
                         if (active) {
-                            d3.selectAll(`.${datasets[i].replaceAll(' ','')}-path`).attr("opacity", 0)
-                            d3.selectAll(`.${datasets[i].replaceAll(' ','')}-legRect`).attr("fill", "white")
+                            d3.selectAll(`.${datasets[i].replaceAll(' ', '')}-path`).attr('opacity', 0);
+                            d3.selectAll(`.${datasets[i].replaceAll(' ', '')}-legRect`).attr('fill', 'white');
                         } else {
-                            d3.selectAll(`.${datasets[i].replaceAll(' ','')}-path`).attr("opacity", 1)
-                            d3.selectAll(`.${datasets[i].replaceAll(' ','')}-legRect`).attr("fill", "black")
+                            d3.selectAll(`.${datasets[i].replaceAll(' ', '')}-path`).attr('opacity', 1);
+                            d3.selectAll(`.${datasets[i].replaceAll(' ', '')}-legRect`).attr('fill', 'black');
                         }
-                    
+
                         d.active = active;
                     }
                 })
-                .text(datasets[i])
-        })
-        
-        
-        
-
-
-
+                .text(datasets[i]);
+        });
     }
-  
+
 
     render() {
         return <div id={this.props.plotId} className="plot" />;
