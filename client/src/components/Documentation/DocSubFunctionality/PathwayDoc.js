f74afe8c99fdeb70a0679207b65b51593ddd51df
gangeshberiuhn <gangesh.beri@uhnresearch.ca>
2020-03-18 16:16:33 -0400

    More Documentation changes!

--- client/src/components/Documentation/DocSubFunctionality/PathwayDoc.js
+++ client/src/components/Documentation/DocSubFunctionality/PathwayDoc.js
@@ -2,6 +2,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import img1 from '../../../images/pathway1.png';
+import img2 from '../../../images/pathway2.png';
+import img3 from '../../../images/pathway3.png';
 
 const PathwayDoc = () => (
     <div className="doc">
@@ -9,6 +11,14 @@ const PathwayDoc = () => (
         <p>All pathways included in ToxicoDB can be explored by clicking on Pathways icon in the top right corner.</p>
         <Link to="/">
             <img className="doc-img" src={img1} alt="" />
+        </Link>
+        <p>This displays a heatmap of the most most significant pathways and drugs from TGGATEs Human Dataset.</p>
+        <Link to="/pathways">
+            <img className="doc-img" src={img2} alt="" />
+        </Link>
+        <p>The selection allows the user to select the dataset then a list of drugs or a single drug, then choose one of the ontologies and a list of pathways which will display a heatmap of the data.</p>
+        <Link to="/pathways">
+            <img className="doc-img last" src={img3} alt="" />
         </Link>
     </div>
 );
