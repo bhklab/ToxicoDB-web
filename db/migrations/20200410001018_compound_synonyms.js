edd4df6073d9058cd610146b2e7fc2db38a316fb
gangeshberiuhn <gangesh.beri@uhnresearch.ca>
2020-04-10 00:28:24 -0400

    migration files to create new tables.

---
+++ db/migrations/20200410001018_compound_synonyms.js
@@ -0,0 +1,20 @@
+
+/* eslint-disable func-names */
+/* eslint-disable no-unused-vars */
+
+exports.up = function (knex, Promise) {
+    return knex.schema.createTable('compound_synonyms', (table) => {
+        table.integer('compound_id')
+            .notNullable()
+            .unsigned()
+            .references('id')
+            .inTable('compounds')
+            .index();
+        table.string('synonym')
+            .notNullable();
+    });
+};
+
+exports.down = function (knex, Promise) {
+    return knex.schema.dropTable('compound_synonyms');
+};
---
+++ db/migrations/20200410001027_gene_synonyms.js
@@ -0,0 +1,20 @@
+
+/* eslint-disable func-names */
+/* eslint-disable no-unused-vars */
+
+exports.up = function (knex, Promise) {
+    return knex.schema.createTable('gene_synonyms', (table) => {
+        table.integer('gene_id')
+            .notNullable()
+            .unsigned()
+            .references('id')
+            .inTable('genes')
+            .index();
+        table.string('synonym')
+            .notNullable();
+    });
+};
+
+exports.down = function (knex, Promise) {
+    return knex.schema.dropTable('gene_synonyms');
+};
